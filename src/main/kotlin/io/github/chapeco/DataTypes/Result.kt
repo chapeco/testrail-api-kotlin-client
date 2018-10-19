bd20f0fd94308e61b4fd7fbc3287880e4bc3c6a4
David Brazil Parker <davidparker241@gmail.com>
2018-10-19 13:57:04 -0700

    Added values and variables for Result

--- src/main/kotlin/io/github/chapeco/DataTypes/Result.kt
+++ src/main/kotlin/io/github/chapeco/DataTypes/Result.kt
@@ -1,3 +1,24 @@
 package io.github.chapeco.DataTypes
 
-data class Result(){}
+import io.github.chapeco.Utilities.Timespan
+import io.github.chapeco.Utilities.Timestamp
+
+data class Result
+(
+    //Read Only
+    val createdBy: Int? = null,
+    val createdOn: Timestamp? = null,
+    val id: Int? = null,
+    val testId: Int? = null,
+
+    //Read/Write
+    var statusId: Int? = null,
+    var comment: String? = null,
+    var version: String? = null,
+    var elapsed: Timespan? = null,
+    var defects: String? = null,
+    var assignedToId: Int? = null
+)
+{
+
+}
