53384da6c7a2cd011d4f451fdaa726abbf51e258
David Brazil Parker <davidparker241@gmail.com>
2018-10-19 12:49:40 -0700

    Added variables and values for Plan, Priority, and Project

--- src/main/kotlin/io/github/chapeco/DataTypes/Plan.kt
+++ src/main/kotlin/io/github/chapeco/DataTypes/Plan.kt
@@ -1,3 +1,44 @@
 package io.github.chapeco.DataTypes
 
-data class Plan(){}
+import io.github.chapeco.Utilities.Timestamp
+
+data class Plan
+(
+    //Read Only
+    val assignedToId: Int? = null,
+    val blockedCount: Int? = null,
+    val completedOn: Timestamp? = null,
+    val createdBy: Int? = null,
+    val createdOn: Timestamp? = null,
+    val customStatusCount: Int? = null,
+    val failedCount: Int? = null,
+    val id: Int? = null,
+    val isCompleted: Boolean? = null,
+    val passedCount: Int? = null,
+    val projectId: Int? = null,
+    val retestCount: Int? = null,
+    val untestedCount: Int? = null,
+    val url: String? = null,
+
+    //Read/Write
+    var name: String,
+    var description: String? = null,
+    var milestoneId: Int? = null,
+    var entries: Array<PlanEntry>? = null
+)
+{
+    data class PlanEntry
+    (
+        var suiteId: Int? = null,
+        var name: String? = null,
+        var description: String? = null,
+        var assignedToId: Int? = null,
+        var includeAll: Boolean? = null,
+        var caseIds: Array<Int>? = null,
+        var configIds: Array<Int>? = null,
+        var runs: Array<Run>? = null
+    )
+    {
+
+    }
+}
--- src/main/kotlin/io/github/chapeco/DataTypes/Priority.kt
+++ src/main/kotlin/io/github/chapeco/DataTypes/Priority.kt
@@ -1,3 +1,11 @@
 package io.github.chapeco.DataTypes
 
-data class Priority(){}
+data class Priority
+(
+    val id: Int? = null,
+    val isDefault: Boolean? = null,
+    val name: String? = null,
+    val priority: Int? = null,
+    val shortName: String? = null
+)
+{}
