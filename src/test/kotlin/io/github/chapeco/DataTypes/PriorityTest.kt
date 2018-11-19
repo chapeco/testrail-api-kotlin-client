f8a42772bd69da6284f559289e7c2cf4f8646680
David Brazil Parker <davidparker241@gmail.com>
2018-11-19 14:16:39 -0800

    Unit tests for Priority data type completed

--- src/test/kotlin/io/github/chapeco/DataTypes/PriorityTest.kt
+++ src/test/kotlin/io/github/chapeco/DataTypes/PriorityTest.kt
@@ -1,6 +1,7 @@
 package io.github.chapeco.DataTypes
 
 import kotlinx.serialization.json.JSON
+import kotlinx.serialization.list
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -57,6 +58,18 @@ class PriorityTest
     @Test
     fun getPrioritiesTest()
     {
-
+        // {id:1,is_default:false,name:Low,priority:1,short_name:Low},
+        // {id:2,is_default:true,name:Medium,priority:2,short_name:Medium},
+        // {id:3,is_default:false,name:High,priority:3,short_name:High},
+        // {id:4,is_default:false,name:Critical,priority:4,short_name:Critical}
+        val expectedPrioritiesList = listOf(
+                Priority(1,false,"Low",1,"Low"),
+                Priority(2,true,"Medium",2,"Medium"),
+                Priority(3,false,"High",3,"High"),
+                Priority(4,false,"Critical",4,"Critical")
+        )
+        val actualPrioritiesList = Priority().getPriorities()
+        println(JSON.unquoted.stringify(Priority.serializer().list,actualPrioritiesList))
+        Assertions.assertEquals(expectedPrioritiesList,actualPrioritiesList)
     }
 }
